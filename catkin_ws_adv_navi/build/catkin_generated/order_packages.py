5632d7681751fce0f14092a13882b520800801d4
jmcmahon443 <jmcmahon443@gmail.com>
2016-04-05 14:16:49 -0400

    deleted old folder

--- catkin_ws_adv_navi/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/vishnu/Desktop/catkin_ws_adv_navi/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/vishnu/Desktop/catkin_ws_adv_navi/devel;/home/vishnu/Desktop/catkin_ws1/devel;/opt/ros/hydro".split(';') if "/home/vishnu/Desktop/catkin_ws_adv_navi/devel;/home/vishnu/Desktop/catkin_ws1/devel;/opt/ros/hydro" != "" else []
--- catkin_ws_adv_navi/build/reconstruction/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vishnu/Desktop/catkin_ws_adv_navi/src/reconstruction
-# Build directory: /home/vishnu/Desktop/catkin_ws_adv_navi/build/reconstruction
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
